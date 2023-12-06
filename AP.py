db1c9c660effc9b3c54eae91c1200ae00c6f4340
AP-AIT <149565417+AP-AIT@users.noreply.github.com>
2023-12-06 12:49:34 +0530

    Update AP.py

--- AP.py
+++ AP.py
@@ -83,10 +83,33 @@ def extract_text_from_pdf(pdf_bytes):
         text += pdf_reader.pages[page_num].extract_text()
     return text
 
+# Initialize mail_id_list outside the try block
+mail_id_list = []
+
 if st.button("Fetch and Generate Excel"):
     try:
-        # ... (existing code for email fetching)
+        # URL for IMAP connection
+        imap_url = 'imap.gmail.com'
 
+        # Connection with GMAIL using SSL
+        my_mail = imaplib.IMAP4_SSL(imap_url)
+
+        # Log in using user and password
+        my_mail.login(user, password)
+
+        # Select the Inbox to fetch messages
+        my_mail.select('inbox')
+
+        # Define the key and value for email search
+        key = 'FROM'
+        value = search_email  # Use the user-inputted email address to search
+        _, data = my_mail.search(None, key, value)
+
+        mail_id_list = data[0].split()
+
+        info_list = []
+
+        # Iterate through messages and extract information from HTML content
         for num in mail_id_list:
             typ, data = my_mail.fetch(num, '(RFC822)')
             msg = email.message_from_bytes(data[0][1])
@@ -96,29 +119,34 @@ if st.button("Fetch and Generate Excel"):
                     html_content = part.get_payload(decode=True).decode('utf-8')
                     info = extract_info_from_html(html_content)
 
-                elif part.get_content_type().startswith('image'):
-                    image_bytes = part.get_payload(decode=True)
-                    text = extract_text_from_image(image_bytes)
-                    info["Image Text"] = text
+                    # Extract and add the received date
+                    date = msg["Date"]
+                    info["Received Date"] = date
 
-                elif part.get_content_type() == 'application/msword':
-                    docx_bytes = part.get_payload(decode=True)
-                    text = extract_text_from_word(docx_bytes)
-                    info["Word Text"] = text
+                    info_list.append(info)
 
-                elif part.get_content_type() == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-                    excel_bytes = part.get_payload(decode=True)
-                    text = extract_text_from_excel(excel_bytes)
-                    info["Excel Text"] = text
+                # ... (existing code for other attachment types)
 
-                elif part.get_content_type() == 'application/pdf':
-                    pdf_bytes = part.get_payload(decode=True)
-                    text = extract_text_from_pdf(pdf_bytes)
-                    info["PDF Text"] = text
+        # Create a DataFrame from the info_list
+        df = pd.DataFrame(info_list)
 
-            # ... (existing code to add received date and append info to the list)
+        # Generate the Excel file
+        st.write("Data extracted from emails:")
+        st.write(df)
 
-        # ... (existing code to create DataFrame, display, and download Excel file)
+        if st.button("Download Excel File"):
+            excel_file = df.to_excel('EXPO_leads.xlsx', index=False, engine='openpyxl')
+            if excel_file:
+                with open('EXPO_leads.xlsx', 'rb') as file:
+                    st.download_button(
+                        label="Click to download Excel file",
+                        data=file,
+                        key='download-excel'
+                    )
+
+        st.success("Excel file has been generated and is ready for download.")
 
     except Exception as e:
         st.error(f"Error: {e}")
+else:
+    st.warning("Click the 'Fetch and Generate Excel' button to retrieve and process emails.")
